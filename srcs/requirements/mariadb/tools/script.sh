96c1e1ec9e389a13bccaad5659fad101b9a9d03e
Alexandre Couture <acouture@c3r8p3.42quebec.com>
2024-01-25 17:27:08 -0500

    working

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,10 +4,6 @@ log_success() {
   echo -e "\033[32m$1\033[0m"  # Vert
 }
 
-log_wait() {
-  echo -e "\033[33m$1\033[0m"  # Jaune
-}
-
 log_info() {
   echo -e "\033[34m$1\033[0m"  # Bleu
 }
@@ -28,13 +24,18 @@ log_success "Mariadb is up and running."
 
 
 sleep 1
+log_info "Creating database..."
 mysql -u root -p  -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+log_success "Database created!"
 sleep 1
+log_info "Creating user..."
 mysql -u root -p  -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'wordpress.srcs_inception' IDENTIFIED BY '${DB_PASSWORD}';"
 mysql -u root -p  -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'wordpress.srcs_inception';"
+log_success "Users created!"
 sleep 1
+log_info "Flushing privileges..."
 mysql -u root -p  -e "FLUSH PRIVILEGES"
-
+log_success "Privileges flushed!"
 sleep 1
 log_info "Stopping mariadb services..."
 mysqladmin -u root -p  shutdown
